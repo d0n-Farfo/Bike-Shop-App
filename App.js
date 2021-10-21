5fec601b5e4d9db585b64551cfdbffa53b573f58
NAA <donald_farfo4100@yahoo.com>
2021-10-21 21:18:56 +0000

    login screen update

--- App.js
+++ App.js
@@ -1,22 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
-import {AntDesign} from '@expo/vector-icons';
-/*
+//import {AntDesign} from '@expo/vector-icons';
 import {createStackNavigator} from '@react-navigation/stack'
 import { NavigationContainer } from '@react-navigation/native';
 import login from './screens/login';
 import home from './screens/home';
-*/
 
-/*
+
+
 export default function App() {
     const mainNavigator = createStackNavigator();
  
   return (
     <View style ={{flex: 1}}> 
         <NavigationContainer>
-          <mainNavigator.Navigator initialRouteName="login">
+          <mainNavigator.Navigator screenOptions={{headerShown: false}} initialRouteName="login">
               <mainNavigator.Screen name="login" component ={login} />
               <mainNavigator.Screen name="home" component ={home} />
           </mainNavigator.Navigator>
@@ -24,8 +23,9 @@ export default function App() {
     </View>
   );
 }
-*/
 
+
+/*
 export default function App(){
   return (
     <View style={styles.container}>
@@ -145,3 +145,4 @@ const styles = StyleSheet.create({
     flexDirection:"row",
   },
 });
+*/
